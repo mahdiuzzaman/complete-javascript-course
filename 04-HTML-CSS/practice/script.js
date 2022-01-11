0ca9e23d774857573a8bd0b7835f20e18fde2e3b
Mahdi Uz Zaman <mahdi.u.zaman@gmail.com>
2022-01-11 18:19:05 +0800

    save files

---
+++ 04-HTML-CSS/practice/script.js
@@ -0,0 +1,9 @@
+console.log();
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+}
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+}
