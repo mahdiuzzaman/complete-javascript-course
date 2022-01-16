70179028907a2574872c17adcbf6ea277b9886a8
Mahdi Uz Zaman <mahdi.u.zaman@gmail.com>
2022-01-16 17:05:39 +0800

    save files

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,14 @@
+/* eslint-disable prefer-rest-params */
+/* eslint-disable object-shorthand */
+/* eslint-disable func-names */
+
 'use strict';
+
+function add(a, b) {
+    console.log(`Sum of a nd b is = ${a + b}`);
+    for (let i = 0; i < arguments.length; i++) {
+        console.log(arguments[i]);
+    }
+}
+
+add(1, 2, 3, 4, 5, 6);
--- 09-Data-Structures-Operators/starter/test.js
+++ 09-Data-Structures-Operators/starter/test.js
@@ -1,10 +1,5 @@
 'use strict';
 
-// Data needed for a later exercise
-const flights =
-    '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-
-// Data needed for first part of the section
 const restaurant = {
     name: 'Classico Italiano',
     location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -27,3 +22,7 @@ const restaurant = {
         },
     },
 };
+
+const { openingHours } = restaurant;
+const { fri: { open, close } } = openingHours;
+console.log(open, close);
